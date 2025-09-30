e3fd335093a709126876ef7f4e0e398aebb84ddc
Alex Barbosa Felix Da Silva <alebarbo@c1r6s8.42porto.com>
2025-09-30 14:21:29 +0100

    added header files

---
+++ incs/ft_minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_minishell.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 01:44:19 by alebarbo          #+#    #+#             */
+/*   Updated: 2025/09/30 14:21:08 by alebarbo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MINISHELL_H
+# define FT_MINISHELL_H
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "ft_error_msgs.h"
+# include "ft_esc_sequences.h"
+
+#endif
