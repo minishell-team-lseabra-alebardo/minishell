d5c073f2c082f43cae86742cc46b43f7586240d5
Lucca Cruz <lucca1609.ls@gmail.com>
2025-10-15 15:40:48 +0100

    feat: MACROS for operators.

--- incs/ft_macros.h
+++ incs/ft_macros.h
@@ -1,16 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_esc_sequences.h                                 :+:      :+:    :+:   */
+/*   ft_macros.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 01:46:44 by alebarbo          #+#    #+#             */
-/*   Updated: 2025/09/30 16:01:17 by alebarbo         ###   ########.fr       */
+/*   Updated: 2025/10/15 15:40:32 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MACROS_H
 # define FT_MACROS_H
+
+// Shell command operators
+# define CMD_AND       "&&"
+# define CMD_OR        "||"
+# define CMD_PIPE      "|"
+# define CMD_HEREDOC   "<<"
+# define CMD_IN        "<"
+# define CMD_OUT       ">"
+# define CMD_OUT_APPEND">>"
 
 #endif
