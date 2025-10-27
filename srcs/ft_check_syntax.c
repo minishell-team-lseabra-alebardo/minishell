721a6841ba33dff1e5a924df1922e0eb5d90aef5
Magusk Lutus <26604686-Magusk89@users.noreply.gitlab.com>
2025-10-27 21:33:17 +0000

    feat: added ft_check_empty_parentheses() and ft_check_invalid_parentheses, changed ft_check_commands() into ft_check_syntax()

--- srcs/ft_check_syntax.c
+++ srcs/ft_check_syntax.c
@@ -6,7 +6,7 @@
 /*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 15:29:12 by alebarbo          #+#    #+#             */
-/*   Updated: 2025/10/23 16:22:29 by alebarbo         ###   ########.fr       */
+/*   Updated: 2025/10/27 21:31:51 by alebarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,45 @@ static char	*ft_skip_quotes(char *line, char c)
 	temp = line;
 	temp++;
 	while (*temp && *temp != c)
+		temp++;
+	return (temp);
+}
+
+static char	*ft_skip_open_parentheses(char *line)
+{
+	char	*temp;
+
+	temp = ft_skip_whitespaces(line);
+	while (*temp == '(')
+	{
+		temp++;
+		temp = ft_skip_whitespaces(temp);
+	}
+	return (temp);
+}
+
+static char	*ft_skip_and(char *line)
+{
+	char	*temp;
+
+	temp = line;
+	temp += 2;
+	temp = ft_skip_whitespaces(temp);
+	if (*temp == '(')
+		temp++;
+	return (temp);
+}
+
+static char	*ft_skip_pipe_or(char *line)
+{
+	char	*temp;
+
+	temp = line;
+	temp++;
+	if (*temp == '|')
+		temp++;
+	temp = ft_skip_whitespaces(temp);
+	if (temp == '(')
 		temp++;
 	return (temp);
 }
@@ -60,18 +99,54 @@ int	ft_check_operators(char *line)
 	return (0);
 }
 
-int	ft_check_commands(char *line)
+int	ft_check_empty_parentheses(char *line)
 {
 	char	*temp;
 
 	temp = ft_skip_whitespaces(line);
-	return (0);	
+	while (*temp)
+	{
+		if (*temp == '(')
+		{
+			temp = ft_skip_whitespaces(temp);
+			if (*temp == ')')
+				return (-1);
+		}
+		if (*temp == '\'' || *temp == '\"')
+			temp = ft_skip_quotes(temp, *temp);
+		temp++;
+	}
+	return (0);
+}
+
+int	ft_check_invalid_parentheses(char *line)
+{
+	char	*temp;
+
+	temp = ft_skip_whitespaces(line);
+	while (*temp)
+	{
+		temp = ft_skip_open_parentheses(temp);
+		if (*temp == '\'' || *temp == '\"')
+			temp = ft_skip_quotes(temp, *temp);
+		if (*temp == '&' && *(temp + 1) == '&')
+			temp = ft_skip_and(temp);
+		else if (*temp == '|')
+			temp = ft_skip_pipe_or(temp);
+		else if (*temp == '(')
+			return (-1);
+		else
+			temp++;
+	}
+	return (0);
 }
 
 int	ft_check_syntax(char *line)
 {
-	if (ft_check_unclosed(line) < 0 || ft_check_operators(line) < 0
-		|| ft_check_commands(line) < 0)
+	if (ft_check_unclosed(line) < 0
+		|| ft_check_operators(line) < 0
+		|| ft_check_empty_parentheses(line) < 0
+		|| ft_check_invalid_parentheses(line) < 0)
 		return (-1);
 	return (0);
 }
