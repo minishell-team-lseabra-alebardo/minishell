0c4580ed3ecb35e56a438f953f00815795bb5da2
Lucca Cruz <lucca1609.ls@gmail.com>
2025-11-07 14:15:18 +0000

    feat: ft_resolve_cmd_path function.

---
+++ srcs/ft_resolve_cmd_path.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_resolve_cmd_path.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 12:45:27 by lseabra-          #+#    #+#             */
+/*   Updated: 2025/11/07 14:00:52 by lseabra-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_minishell.h>
+
+static void	ft_free_strarr(char **strarr)
+{
+	int	i;
+
+	i = 0;
+	while (strarr[i])
+	{
+		free(strarr[i]);
+		i++;
+	}
+	free(strarr);
+}
+
+static int	ft_check_abs_path(char *cmd, char **path_addr)
+{
+	if (access(cmd, F_OK | X_OK) == 0)
+	{
+		*path_addr = ft_strdup(cmd);
+		return (EXIT_SUCCESS);
+	}
+	else if (access(cmd, F_OK) == 0)
+		return (EXIT_CANNOT_EXEC);
+	else
+		return (EXIT_NOT_FOUND);
+}
+
+static int	ft_search_in_path(char *cmd, char **path_addr, char **path_arr)
+{
+	int		i;
+	int		status;
+	char	*dir_path;
+	char	*complete_path;
+
+	status = EXIT_NOT_FOUND;
+	i = 0;
+	while (path_arr[i])
+	{
+		dir_path = ft_strjoin(path_arr[i], "/");
+		complete_path = ft_strjoin(*dir_path, cmd);
+		free(dir_path);
+		if (access(complete_path, F_OK | X_OK) == 0)
+		{
+			*path_addr = complete_path;
+			return (EXIT_SUCCESS);
+		}
+		else if (access(complete_path, F_OK) == 0 && status == EXIT_NOT_FOUND)
+			status = EXIT_CANNOT_EXEC;
+		free(complete_path);
+		i++;
+	}
+	return (status);
+}
+
+int	ft_resolve_cmd_path(char *cmd, char **path_addr)
+{
+	char	**path_arr;
+	char	*path;
+	int		status;
+
+	if (ft_strchr(cmd, '/'))
+		return (ft_check_abs_path(cmd, &path));
+	path_arr = ft_split(getenv("PATH"), ':');
+	status = ft_search_in_path(cmd, path_addr, path_arr);
+	ft_free_strarr(path_arr);
+	return (status);
+}
