fb0f8fb24cd9d203ee9f3cedd28a9dd0ae6ac766
Magusk Lutus <26604686-Magusk89@users.noreply.gitlab.com>
2025-11-08 23:26:25 +0000

    norm: reduced number of lines in main()

--- srcs/ft_minishell.c
+++ srcs/ft_minishell.c
@@ -6,7 +6,7 @@
 /*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 21:46:59 by alebarbo          #+#    #+#             */
-/*   Updated: 2025/11/07 21:50:43 by alebarbo         ###   ########.fr       */
+/*   Updated: 2025/11/08 23:23:43 by alebarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,18 @@ static void	ft_free_ms_envp(char **ms_envp)
 	free(ms_envp);
 }
 
+static void	ft_close_error(t_data *dt)
+{
+	perror(strerror(errno));
+	if (dt)
+	{
+		if (dt->ms_envp)
+			ft_free_ms_envp(dt->ms_envp);
+		free(dt);
+	}
+	exit(EXIT_FAILURE);
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_data	*dt;
@@ -32,22 +44,15 @@ int	main(int argc, char *argv[], char *envp[])
 	(void) argc;
 	(void) argv;
 	if (ft_listener() < 0)
-	{
-		perror(strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	dt = ft_calloc(1, sizeof(t_data));
+		ft_close_error(0);
+	dt = (t_data *) ft_calloc(1, sizeof(t_data));
 	if (!dt)
 		return (EXIT_FAILURE);
 	dt->ms_envp = ft_strarr_dup(envp);
 	if (!dt->ms_envp)
 		return (EXIT_FAILURE);
 	if (ft_ms_path(dt) < 0)
-	{
-		ft_free_ms_envp(dt->ms_envp);
-		free(dt);
-		return (EXIT_FAILURE);
-	}
+		ft_close_error(dt);
 	dt->lst_stat = 0;
 	dt->cmd_ll = NULL;
 	ft_read_line(dt);
