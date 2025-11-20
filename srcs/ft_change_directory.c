d797b4f940e7cfc2796366f874798e470977dbe3
Lucca Cruz <lucca1609.ls@gmail.com>
2025-11-20 18:45:03 +0000

    feat: ft_change_directory without before test.

---
+++ srcs/ft_change_directory.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_change_directory.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/19 16:53:43 by lseabra-          #+#    #+#             */
+/*   Updated: 2025/11/20 18:44:37 by lseabra-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_minishell.h>
+
+static char	**ft_getenv(char *name, char **ms_envp)
+{
+	size_t	len;
+	int		i;;
+	
+	if (!name || !ms_envp)
+		return (NULL);
+	len = ft_strlen(name);
+	i = 0;
+	while (ms_envp[i])
+	{
+		if (ft_strncmp(name, ms_envp[i], len) == 0 && ms_envp[i][len] == '=')
+			return (ms_envp + i);
+		i++;
+	}
+	return (NULL);
+}
+
+static void	ft_set_env(char *name, char *value, char **ms_envp)
+{
+	char	*res;
+	char	*tmp;
+	char	**env_addr;
+	size_t	name_len;
+
+	if (!name || !value || !ms_envp || !*ms_envp)
+		return ;
+	name_len = 0;
+	env_addr = ft_getenv(name, ms_envp);
+	if (!env_addr)
+		return ;
+	while ((*env_addr)[name_len] && (*env_addr)[name_len] != "=")
+		name_len++;
+	if ((*env_addr)[name_len] == "=")
+		name_len++;
+	else
+		return ;
+	tmp = ft_substr(*env_addr, 0, name_len);
+	res = ft_strjoin(tmp, value);
+	free(tmp);
+	tmp = *env_addr;
+	*env_addr = res;
+	free(tmp);
+}
+
+void	ft_change_directory(t_data *dt, char **args)
+{
+	char	*pwd;
+	char	*path;
+	char	*tmp;
+	int		i;
+
+	
+	if (ft_strncmp(args[0], "cd", 3) != 0)
+		return ;
+	pwd = getcwd(0, 0);
+	i = 0;
+	while (args[i])
+		i++;
+	if (chdir(args[1]) < 0)
+	{
+		free(pwd);
+		ft_puterror_exit(args[1], strerror(errno), EXIT_FAILURE);
+	}
+	ft_set_env("OLD_PWD", pwd, dt->ms_envp);
+	free(pwd);
+	pwd = getcwd(0, 0);
+	ft_set_env("PWD", pwd, dt->ms_envp);
+	free(pwd);
+}
