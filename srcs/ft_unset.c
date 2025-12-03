cc87167eb6616568e1f1bba05d303df0e60a9dd0
Magusk Lutus <26604686-Magusk89@users.noreply.gitlab.com>
2025-12-03 04:22:56 +0000

    norm: created function ft_remove_variable() to reduce ft_unset() number of lines, complying to the norm

--- srcs/ft_unset.c
+++ srcs/ft_unset.c
@@ -6,17 +6,29 @@
 /*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 21:51:58 by alebarbo          #+#    #+#             */
-/*   Updated: 2025/12/02 22:13:21 by alebarbo         ###   ########.fr       */
+/*   Updated: 2025/12/03 04:21:55 by alebarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_minishell.h>
 
+static void	ft_remove_variable(t_data *dt, int j)
+{
+	char	*temp;
+
+	temp = dt->ms_envp[j];
+	while (dt->ms_envp[j])
+	{
+		dt->ms_envp[j] = dt->ms_envp[j + 1];
+		j++;
+	}
+	free(temp);
+}
+
 void	ft_unset(t_data *dt, char **args)
 {
 	int		i;
 	int		j;
-	char	*temp;
 
 	i = 1;
 	while (args[i])
@@ -28,13 +40,7 @@ void	ft_unset(t_data *dt, char **args)
 				&& (dt->ms_envp[j][ft_strlen(args[i])] == '='
 				|| !dt->ms_envp[j][ft_strlen(args[i])]))
 			{
-				temp = dt->ms_envp[j];
-				while (dt->ms_envp[j])
-				{
-					dt->ms_envp[j] = dt->ms_envp[j + 1];
-					j++;
-				}
-				free(temp);
+				ft_remove_variable(dt, j);
 				break ;
 			}
 			j++;
