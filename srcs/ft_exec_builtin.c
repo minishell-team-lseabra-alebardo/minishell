e03512b97128f820df429d1dd6dc215fbcceb2e5
Lucca Cruz <lucca1609.ls@gmail.com>
2025-12-04 12:54:07 +0000

    doc: add doxygen documentation and fix the return of a pipeline builtin from EXIT_SUCCESS to lst_stat.

--- srcs/ft_exec_builtin.c
+++ srcs/ft_exec_builtin.c
@@ -3,15 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exec_builtin.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 11:44:08 by lseabra-          #+#    #+#             */
-/*   Updated: 2025/12/02 22:27:01 by alebarbo         ###   ########.fr       */
+/*   Updated: 2025/12/04 12:53:17 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_minishell.h>
 
+/**
+ * @brief Checks if a command is part of a pipeline.
+ *
+ * This function determines whether the given command is connected to other
+ * commands via pipe operators. A command is considered to be in a pipeline
+ * if either its previous operator is a pipe (CMD_PIPE) or if it has a next
+ * command whose previous operator is a pipe.
+ *
+ * @param cmd Pointer to the command structure to check.
+ * @return true if the command is part of a pipeline, false otherwise.
+ */
 bool	ft_is_in_pipeline(t_cmd *cmd)
 {
 	if (!cmd)
@@ -24,6 +35,17 @@ bool	ft_is_in_pipeline(t_cmd *cmd)
 		return (false);
 }
 
+/**
+ * @brief Checks if a command is a parent-context builtin.
+ *
+ * Parent-context builtins are commands that must be executed in the parent
+ * shell process because they modify the shell's environment or state. These
+ * include: cd (changes directory), exit (terminates shell), export (sets
+ * environment variables), and unset (removes environment variables).
+ *
+ * @param cmd String containing the command name to check.
+ * @return true if the command is a parent-context builtin, false otherwise.
+ */
 bool	ft_is_parent_bltn(char *cmd)
 {
 	if (!cmd)
@@ -40,6 +62,16 @@ bool	ft_is_parent_bltn(char *cmd)
 		return (false);
 }
 
+/**
+ * @brief Checks if a command is a builtin command.
+ *
+ * This function determines whether the given command name corresponds to
+ * any of the shell's builtin commands, including both parent-context builtins
+ * (cd, exit, export, unset) and child-compatible builtins (pwd, echo, env).
+ *
+ * @param cmd String containing the command name to check.
+ * @return true if the command is a builtin, false otherwise.
+ */
 bool	ft_is_builtin(char *cmd)
 {
 	if (!cmd)
@@ -56,6 +88,28 @@ bool	ft_is_builtin(char *cmd)
 		return (false);
 }
 
+/**
+ * @brief Executes a builtin command.
+ *
+ * This function handles the execution of all builtin shell commands. It
+ * first applies any input/output redirections specified for the command.
+ * If redirections succeed, it duplicates the appropriate file descriptors
+ * for stdin/stdout, then dispatches to the specific builtin handler based
+ * on the command name.
+ *
+ * After execution, file descriptors are restored to their original state.
+ * If the command is part of a pipeline, the function terminates the child
+ * process with lst_stat after execution.
+ *
+ * Supported builtins: cd, exit, pwd, export, echo, unset, env.
+ *
+ * @param dt Pointer to the main data structure containing shell state and
+ *           environment.
+ * @param cmd Pointer to the command structure containing arguments and
+ *            redirections.
+ * @return The exit status of the builtin command (EXIT_SUCCESS or
+ *         EXIT_FAILURE).
+ */
 int	ft_exec_builtin(t_data *dt, t_cmd *cmd)
 {
 	int	lst_stat;
@@ -81,6 +135,6 @@ int	ft_exec_builtin(t_data *dt, t_cmd *cmd)
 		ft_env(dt->ms_envp);
 	ft_reset_dup2(cmd);
 	if (ft_is_in_pipeline(cmd))
-		exit(EXIT_SUCCESS);
+		exit(lst_stat);
 	return (lst_stat);
 }
