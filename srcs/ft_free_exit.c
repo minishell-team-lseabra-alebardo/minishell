f36f8c39758b2443cdd724d2e154cdd6c8b8324c
Lucca Cruz <lucca1609.ls@gmail.com>
2025-12-11 16:43:32 +0000

    fix: invalid read (use after free).

--- srcs/ft_free_exit.c
+++ srcs/ft_free_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_free_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 22:25:09 by alebarbo          #+#    #+#             */
-/*   Updated: 2025/12/09 21:36:06 by alebarbo         ###   ########.fr       */
+/*   Updated: 2025/12/11 16:41:03 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,11 +76,11 @@ int	ft_exit(t_data *dt, t_cmd *cmd)
 	ft_close_unused_fds(dt->cmd_ll);
 	ft_free_strarr(dt->ms_envp);
 	ft_free_prompt_line(dt);
+	if (pexit && (!cmd || (cmd && !ft_is_in_pipeline(cmd))))
+		printf("exit\n");
 	ft_cleanup_line(dt);
 	free(dt);
 	rl_clear_history();
-	if (pexit && (!cmd || (cmd && !ft_is_in_pipeline(cmd))))
-		printf("exit\n");
 	if (ft_get_status(0, false) == EXIT_SUCCESS)
 		exit(EXIT_SUCCESS);
 	exit(EXIT_FAILURE);
